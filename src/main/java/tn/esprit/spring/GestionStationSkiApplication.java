4e55294642ead11c5f4e64be97996139c56add92
Youssef ALMIA <almia.youssef2@gmail.com>
2023-11-13 22:20:18 +0100

    optimizations

--- src/main/java/tn/esprit/spring/GestionStationSkiApplication.java
+++ src/main/java/tn/esprit/spring/GestionStationSkiApplication.java
@@ -8,8 +8,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class GestionStationSkiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestionStationSkiApplication.class, args);
-	}
-	
+    public static void main(String[] args) {
+        SpringApplication.run(GestionStationSkiApplication.class, args);
+    }
+
 }
